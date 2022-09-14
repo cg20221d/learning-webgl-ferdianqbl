8b4f93426f02b816910a5a9a868bd50289bbd7cd
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-09-14 14:52:09 +0700

    bug fixing

--- meeting-2/script.js
+++ meeting-2/script.js
@@ -10,8 +10,9 @@ void main() {
   float x = 0.0;
   float y = 0.0;
   float z = 0.0;
-  gl_PointSize = 20.0;
-  gl_Position = vec4(x, y, z, 1.0);
+  float w = 1.0;
+  gl_PointSize = 50.0;
+  gl_Position = vec4(x, y, z, w);
   
 }
 `;
@@ -22,11 +23,12 @@ void main() {
 
   // Fragment shader --> untuk warna
   const fragmentShaderCode = `
+  precision lowp float;
 void main() {
-  // float r = 0.0;
-  // float g = 0.0;
-  // float b = 1.0;
-  gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+  float r = 0.0;
+  float g = 0.0;
+  float b = 0.0;
+  gl_FragColor = vec4(r, g, b, 1.0);
 }
 `;
 
